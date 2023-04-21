29d93e2ff8ba2be4e38769a6e9982b9d671f6ba8
Nam Công <namcong.hnue@gmail.com>
2023-04-21 15:22:38 +0700

    Popup Login to Proceed to Checkout

---
+++ app/code/SmartOSC/CustomInvoiceEmail/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+let config = {
+    'config': {
+        'mixins': {
+            'Magento_Checkout/js/view/minicart': {
+                'SmartOSC_CustomInvoiceEmail/js/checkout/view/minicart': true
+            }
+        }
+    }
+};
